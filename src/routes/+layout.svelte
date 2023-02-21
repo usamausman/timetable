3899454230ba67acaf9d3780240e9cfefbd96257
usamausman <usamaa12@gmail.com>
2023-02-21 23:14:59 +0000

    CLEANUP: Fix build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
 	import '../global.css';
+	import { base } from '$app/paths';
 </script>
+
+<svelte:head>
+	<link rel="apple-touch-icon" sizes="180x180" href="{base}/icon/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="{base}/icon/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="{base}/icon/favicon-16x16.png" />
+	<link rel="manifest" href="{base}/site.webmanifest" />
+	<link rel="mask-icon" href="{base}/icon/safari-pinned-tab.svg" color="#222222" />
+	<link rel="shortcut icon" href="{base}/favicon.ico" />
+	<meta name="msapplication-TileColor" content="#b91d47" />
+	<meta name="msapplication-config" content="{base}/icon/browserconfig.xml" />
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+</svelte:head>
 
 <slot />
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -3,18 +3,13 @@
 /// <reference lib="esnext" />
 /// <reference lib="webworker" />
 
-import { build, files, version } from '$service-worker';
+import { build, files, prerendered, version } from '$service-worker';
 
-// Create a unique cache name for this deployment
 const CACHE = `cache-${version}`;
 
-const ASSETS = [
-	...build, // the app itself
-	...files // everything in `static`
-];
+const ASSETS = [...build, ...files, ...prerendered];
 
 self.addEventListener('install', (event) => {
-	// Create a new cache and add all files to it
 	async function addFilesToCache() {
 		const cache = await caches.open(CACHE);
 		await cache.addAll(ASSETS);
@@ -24,7 +19,6 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-	// Remove previous cached data from disk
 	async function deleteOldCaches() {
 		for (const key of await caches.keys()) {
 			if (key !== CACHE) await caches.delete(key);
@@ -35,20 +29,17 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-	// ignore POST requests etc
 	if (event.request.method !== 'GET') return;
 
 	async function respond() {
 		const url = new URL(event.request.url);
 		const cache = await caches.open(CACHE);
 
-		// `build`/`files` can always be served from the cache
 		if (ASSETS.includes(url.pathname)) {
 			return cache.match(event.request);
 		}
 
-		// for everything else, try the network first, but
-		// fall back to the cache if we're offline
+		// for everything else, use the network
 		const response = await fetch(event.request);
 		return response;
 	}
